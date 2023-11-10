dc597210ed02e34014f929d2029a38f92005bc7b
TripleJ160 <johanusa16@gmail.com>
2023-11-10 10:07:19 -0500

    Base Portfolio Website
    Reached a good start point.

---
+++ src/data/constants.js
@@ -0,0 +1,193 @@
+export const Bio = {
+  name: "Johan John Joji",
+  roles: [
+    "Computer Engineer @ USF",
+  ],
+  description:
+  "I'm a passionate computer engineering enthusiast with a keen interest in all things tech.",
+  github: "https://github.com/triplej160",
+  resume:
+    "https://drive.google.com/file/d/1-HFa38G_BWJH-IbFEajIzU7bSPxDKVG1/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/johanjohnjoji/",
+  insta: "https://www.instagram.com/__j.o.h.a.n__/",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+  
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Node Js",
+        image: "https://nodejs.org/static/images/logo.svg",
+      },
+      
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+    ],
+  },
+  
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://camo.githubusercontent.com/fbfcb9e3dc648adc93bef37c718db16c52f617ad055a26de6dc3c21865c3321d/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6769742d73636d2f6769742d73636d2d69636f6e2e737667",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+  
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://media1.fdncms.com/memphisflyer/imager/u/original/5224734/usf-bulls-logo-hd-1380x1100.png",
+    role: "Application Specialist",
+    company: "USF Rightpath Research & Innovation Center",
+    date: "Sept 2022 - Current",
+    desc: " Assist with testing and debugging solutions on different devices that helped progression of studies. \n Work with research staff to develop business requirements for research study applications. \n Script and program experimental tests in Millisecond Inquisit using a C-based scripting language. \n Assist with creating and maintaining databases according to specifications using REDCap (research database software).",
+    skills: [
+      "C Scripting",
+      "Millisecond Inquisit",
+      "REDCap Database",
+    ],
+    doc: "https://www.usf.edu/cbcs/cfs/rightpath/index.aspx",
+  },
+  {
+    id: 0,
+    img: "https://th.bing.com/th/id/R.06211893f13b4fb69d4f0947f5d061f9?rik=axPacROLKGSt0A&riu=http%3a%2f%2flofrev.net%2fwp-content%2fphotos%2f2016%2f06%2fwalmart-logo.jpg&ehk=QAPqFa3PkbiPgSZImuNmt4qK5xY6etkwbMpTjptzUao%3d&risl=&pid=ImgRaw&r=0",
+    role: "Electronics Associate",
+    company: "Walmart",
+    date: "June 2021 - August 2022",
+    desc: " Conducted retail sales, operated registers, printers and cameras for passport photography",
+    skills: [
+      "Customer Service",
+      "Retail Sales",
+      "Inventory Management",
+      "Cashier",
+
+    ],
+    doc: "",
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://media1.fdncms.com/memphisflyer/imager/u/original/5224734/usf-bulls-logo-hd-1380x1100.png",
+    school: "University of South Florida",
+    date: "Aug 2021 - Present",
+    grade: "3.7 GPA",
+    desc: "I am currently pursuing a Bachelor's degree in Computer Engineering at University of South Florida. I have taken courses in Data Structures, Algorithms, Computer Logic Design among others.",
+    degree: "Bachelor of Science, Computer Engineering",
+  },
+];
+
+export const projects = [
+  {
+    id: 9,
+    title: "Resume Resurrector",
+    date: "October 2023",
+    description:
+      "A web application powered by LangChain and OpenAI's LLM model for resume evaluation and enhancement. Analyze your resume in context with job descriptions and receive feedback for job applications.",
+    image:
+      "https://user-images.githubusercontent.com/83926918/282086129-b51a3d6a-9733-42c4-8344-d2426cb4d0b2.png",
+    tags: [
+      "OpenAI API",
+      "Langchain",
+      "Pandas",
+      "tqdm",
+      "Python",
+      "Streamlit",
+    ],
+    category: "web app",
+    github: "https://github.com/TripleJ160/ResumeResurrector/blob/main/README.md",
+    webapp: "http://resumeresurrector.streamlit.app/",
+    member: [
+      {
+        name: "xxx",
+        img: "xxx",
+        linkedin: "xxx",
+        github: "xxx",
+      },
+      {
+        name: "xxx",
+        img: "xxx",
+        linkedin: "xxx",
+        github: "xxx",
+      },
+    ],
+  },
+  {
+    id: 0,
+    title: "Bible Study App",
+    date: "July 2023",
+    description:
+      "The Bible Study App is an interactive web application that allows users to explore and deepen their understanding of the Christian faith. The app features a chatbot powered by AI, which provides biblical responses to users' questions based solely on passages from the Bible.",
+    image:
+      "https://user-images.githubusercontent.com/83926918/282089111-7cbcaca2-8359-46fd-a2e5-856b3052f04e.png",
+    tags: ["OpenAI API",
+    "Langchain",
+    "Python",
+    "Streamlit",
+    ],
+    category: "web app",
+    github: "https://github.com/TripleJ160/bibleapp",
+    webapp: "https://bibleai.streamlit.app/",
+    
+  },
+];
+
+export const TimeLineData = [
+  { year: 2021, text: "Started my Computer Engineering degree!" },
+];
